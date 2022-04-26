f19c0e743dd181300f92b2fb952896a43c17d775
jouks69 <103070349+jouks69@users.noreply.github.com>
2022-04-26 12:46:13 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express')
+var app = express()
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/html/index.html")
+})
+app.get('/cadastro', (req, res) => {
+    res.sendFile(__dirname + "/html/cadastro.html")
+})
+app.get('/agendamento', (req, res) => {
+    res.sendFile(__dirname + "/html/agendamento.html")
+})
+
+app.listen(3000, () =>{
+    console.log("server run")
+})
